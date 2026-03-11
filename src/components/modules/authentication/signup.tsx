e7a9e16fb2a4a2ec38ae0972bff03aa88e089a10
AnikaTabassum217 <tabassum.anika579@gmail.com>
2026-03-11 20:47:05 +0600

    Sign up done in fronend

--- src/components/modules/authentication/signup.tsx
+++ src/components/modules/authentication/signup.tsx
@@ -5,6 +5,10 @@ import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
+import { useState } from "react";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+
 
 interface Signup1Props {
   heading?: string;
@@ -35,7 +39,34 @@ const Signup = ({
   className,
 }: Signup1Props) => {
 
-  
+
+   const [name,setName] = useState("")
+  const [email,setEmail] = useState("")
+  const [password,setPassword] = useState("")
+  const [confirmPassword,setConfirmPassword] = useState("")
+  const router = useRouter()
+
+    const handleSignup = async () => {
+
+    if(password !== confirmPassword){
+      alert("Passwords do not match")
+      return
+    }
+
+    const data = await authClient.signUp.email({
+      name,
+      email,
+      password,
+      callbackURL: "http://localhost:3000"
+    })
+
+    console.log("Signup result:", data)
+    if(data){
+      router.push('/')
+    }
+    
+  }
+
   return (
     <section className={cn("mt-20", className)}>
       <div className="flex h-full items-center justify-center">
@@ -51,25 +82,39 @@ const Signup = ({
           </a> */}
           <div className="flex w-full max-w-sm min-w-sm flex-col items-center gap-y-4 rounded-md border border-muted bg-background px-6 py-8 shadow-md">
             {heading && <h1 className="text-xl font-semibold">{heading}</h1>}
+             <Input
+              type="text"
+              placeholder="name"
+              className="text-sm"
+              required
+               value={name}
+              onChange={(e)=>setName(e.target.value)}
+            />
             <Input
               type="email"
               placeholder="Email"
               className="text-sm"
               required
+              value={email}
+              onChange={(e)=>setEmail(e.target.value)}
             />
             <Input
               type="password"
               placeholder="Password"
               className="text-sm"
               required
+              value={password}
+              onChange={(e)=>setPassword(e.target.value)}
             />
             <Input
               type="password"
               placeholder="Confirm Password"
               className="text-sm"
               required
+               value={confirmPassword}
+              onChange={(e)=>setConfirmPassword(e.target.value)}
             />
-            <Button type="submit" className="w-full">
+            <Button type="submit" className="w-full" onClick={handleSignup}>
               {buttonText}
             </Button>
           </div>
