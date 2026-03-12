822b147a1bfa3e3b0226cf34b1a3f370c2fc5c44
AnikaTabassum217 <tabassum.anika579@gmail.com>
2026-03-12 19:58:59 +0600

    in proxy file admin,selelr and customer protection is done in frontend

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,26 +3,43 @@ import { userService } from "@/services/user.service";
 import { NextRequest, NextResponse } from "next/server";
 
 
-export async function proxy(request:NextRequest){
-    
-    let isAuthenticated = false;
-    let isAdmin=false;
+export async function proxy(request: NextRequest) {
 
-    const {data} = await userService.getSession()
-    
-    if(data){
-        isAuthenticated = true;
-        isAdmin = data.user.role === Roles.admin
-    }
-    
-    if(!isAuthenticated){
-        return NextResponse.redirect(new URL("/login",request.url))
+    const pathname = request.nextUrl.pathname;
+
+    // let isAuthenticated = false;
+    // let isAdmin = false;
+    // let role;
+
+    const { data } = await userService.getSession()
+
+    // if (data) {
+    //     isAuthenticated = true;
+    //     isAdmin = data.user.role === Roles.admin
+    // }
+
+    if (!data) {
+        return NextResponse.redirect(new URL("/login", request.url))
     }
 
+    const role = data.user.role
+    // ADMIN protection
+    if (pathname.startsWith("/admin-dashboard") && role !== Roles.admin) {
+        return NextResponse.redirect(new URL("/login", request.url));
+    }
+
+    // SELLER protection
+    if (pathname.startsWith("/seller-dashboard") && role !== Roles.seller) {
+        return NextResponse.redirect(new URL("/login", request.url));
+    }
+
+    // CUSTOMER protection
+    if (pathname.startsWith("/dashboard") && role !== Roles.customer) {
+        return NextResponse.redirect(new URL("/login", request.url));
+    }
     return NextResponse.next()
-    
 }
 
-export const config={
-    matcher:["/dashboard"]
+export const config = {
+    matcher: ["/dashboard", "/admin-dashboard", "/seller-dashboard"]
 }
