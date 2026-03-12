9caf675db2b26d0921c7953e9e9361d3506fd807
AnikaTabassum217 <tabassum.anika579@gmail.com>
2026-03-12 15:16:46 +0600

    In proxy.ts file authentication is continoue

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -3,8 +3,8 @@ import { userService } from "@/services/user.service";
 
 export default async function Home() {
   
-  const {data} = await userService.getSession();
-  console.log(data)
+  // const {data} = await userService.getSession();
+  // console.log(data)
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-00 font-sans dark:bg-black">
---
+++ src/proxy.ts
@@ -0,0 +1,28 @@
+import { Roles } from "@/constants/roles";
+import { userService } from "@/services/user.service";
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function proxy(request:NextRequest){
+    
+    let isAuthenticated = false;
+    let isAdmin=false;
+
+    const {data} = await userService.getSession()
+    
+    if(data){
+        isAuthenticated = true;
+        isAdmin = data.user.role === Roles.admin
+    }
+    
+    if(!isAuthenticated){
+        return NextResponse.redirect(new URL("/login",request.url))
+    }
+
+    return NextResponse.next()
+    
+}
+
+export const config={
+    matcher:["/dashboard"]
+}
