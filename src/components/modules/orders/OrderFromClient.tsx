0df43141dd833bc695941b183bb89bfaf9c9f3a1
AnikaTabassum217 <tabassum.anika579@gmail.com>
2026-04-02 09:59:55 +0600

    Review design modified

--- src/components/modules/orders/OrderFromClient.tsx
+++ src/components/modules/orders/OrderFromClient.tsx
@@ -39,12 +39,29 @@ export default function OrderFromClient({
           className="border rounded-xl p-4 shadow-sm"
         >
           <p><strong>Order ID:</strong> {order.id}</p>
-          <p><strong>Status:</strong> {order.status}</p>
+          <p><strong>Status:</strong>
+           {order.status}
+            
+           </p>
           <p><strong>Total:</strong> ৳{order.totalAmount}</p>
           <p><strong>Address:</strong> {order.shippingAddress}</p>
           <Link href={`/orders/${order.id}`}>  <p className="hover:underline"><strong>View Details</strong></p></Link>
           
+{/* {order.status === "DELIVERED" && (
+  <p className="text-sm text-green-600">
+    You can give a review
+  </p>
+)} */}
 
+{order.status === "DELIVERED" ? (
+  <p className="text-green-600 text-sm mt-1">
+     You can review this order
+  </p>
+) : (
+  <p className="text-gray-500 text-sm mt-1">
+    Review available after delivery
+  </p>
+)}
         </div>
       ))}
     </div>
